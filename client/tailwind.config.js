ff804c8c16f9383d5e64ff4f63dfe0fddb1ba847
Teodor Nilseng Danielsen <teodor.danielsen@gmail.com>
2022-01-17 21:35:47 +0100

    upgrading to react-scripts 5

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {
       colors: {
@@ -8,9 +8,6 @@ module.exports = {
         primary: "var(--color-primary)",
       }
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
