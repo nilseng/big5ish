11b573cdd3f2c9114509d05fd9681f02d565ad20
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2022-11-19 12:57:58 +0100

    refactor: extracts player list to it's own component

---
+++ client/src/components/PlayerList.tsx
@@ -0,0 +1,32 @@
+import { gql, useQuery } from "@apollo/client";
+import { Player } from "@big5ish/types";
+
+const playersQuery = gql`
+  query players($gameId: ID!) {
+    players(gameId: $gameId) {
+      id
+      nickname
+    }
+  }
+`;
+
+export const PlayerList = ({ gameId }: { gameId: string }) => {
+  const { data } = useQuery<{ players: Player[] }, { gameId: string }>(playersQuery, {
+    variables: { gameId },
+    pollInterval: 500,
+  });
+
+  return (
+    <div className="flex flex-col items-center w-full max-w-xs mt-10">
+      <h2 className="text-gray-50 text-xl mb-4">Players</h2>
+      <div className="flex flex-col items-center w-full h-60 max-h-60 overflow-scroll bg-gray-50 bg-opacity-10 rounded-xl p-4">
+        {(!data?.players || data?.players.length === 0) && <p className="text-gray-200">No one has joined yet...</p>}
+        {data?.players?.map((player) => (
+          <div key={player.id} className="text-gray-50 text-sm font-bold my-2">
+            {player.nickname}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
--- client/src/components/WaitingRoom.tsx
+++ client/src/components/WaitingRoom.tsx
@@ -3,34 +3,36 @@ import { Player } from "@big5ish/types";
 import { useNavigate, useParams } from "react-router-dom";
 import { paths } from "../config";
 import { NeuButton } from "./NeuButton";
+import { PlayerList } from "./PlayerList";
 
 const playersQuery = gql`
   query players($gameId: ID!) {
     players(gameId: $gameId) {
       id
-      nickname
     }
   }
 `;
 
 export const WaitingRoom = () => {
-  const { roomId } = useParams();
+  const { gameId } = useParams();
   const navigate = useNavigate();
 
-  const { data } = useQuery<{ players: Player[] }, { gameId: string }>(playersQuery, {
-    variables: { gameId: roomId ?? "" },
+  const { data, stopPolling } = useQuery<{ players: Player[] }, { gameId: string | undefined }>(playersQuery, {
+    variables: { gameId },
     pollInterval: 500,
   });
 
+  if (data?.players && data.players.length > 1) stopPolling();
+
   return (
-    <div className="flex flex-col items-center">
+    <>
       <p className="text-gray-200">game id</p>
       <h1 className="text-3xl text-gray-50">
-        <code>{roomId}</code>
+        <code>{gameId}</code>
       </h1>
       <NeuButton
         type="colored"
-        className="mt-10"
+        className="border-4 border-white mt-10"
         textClassName="font-bold text-xl text-gray-50"
         text="PLAY"
         style={{ height: "10rem", width: "10rem", padding: "2.5rem" }}
@@ -44,17 +46,7 @@ export const WaitingRoom = () => {
         </p>
         <p className="text-gray-200 self-end">to join the game</p>
       </div>
-      <div className="flex flex-col items-center w-full max-w-screen-sm mt-10">
-        <h2 className="text-gray-50 text-xl mb-4">Players</h2>
-        <div className="flex flex-col items-center w-full h-60 max-h-60 overflow-scroll bg-gray-50 bg-opacity-10 rounded-xl p-4">
-          {(!data?.players || data?.players.length === 0) && <p className="text-gray-200">No one has joined yet...</p>}
-          {data?.players?.map((player) => (
-            <div key={player.id} className="text-gray-50 text-sm font-bold my-2">
-              {player.nickname}
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+      {gameId && <PlayerList gameId={gameId} />}
+    </>
   );
 };
