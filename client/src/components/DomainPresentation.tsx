02ab79704a1af0d6833d2ee2fad996dfa6a6140c
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2022-12-19 14:31:48 +0100

    styling

--- client/src/components/DomainPresentation.tsx
+++ client/src/components/DomainPresentation.tsx
@@ -3,11 +3,11 @@ import { StepBar } from "./StepBar";
 export const DomainPresentation = () => {
   return (
     <>
-      <StepBar currentStep={0} stepCount={5} />
+      <StepBar currentStep={1} stepCount={5} />
       <div className="flex flex-col items-center my-32">
         <p>Next up...</p>
-        <h1 className="text-5xl font-bold mt-16 mb-32">Neuroticism</h1>
-        <code className="text-5xl">😱 😬 😢 🐥 😡 😰</code>
+        <h1 className="text-5xl mt-16 mb-32">Neuroticism</h1>
+        <code className="text-4xl">😡😬😱</code>
       </div>
     </>
   );
