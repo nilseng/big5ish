40757f5899a634c4b2084e175f7ffcb771e85ad1
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-01-12 12:07:13 +0100

    nit: formatting

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -24,7 +24,7 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className="absolute top-0 w-screen flex items-center flex-wrap p-4">
+    <nav className="w-screen flex items-center flex-wrap p-4">
       <Link to="/" className="flex items-center">
         <NeuButton type="colored" style={{ padding: "1rem" }} componentIcon={<Logo height="2rem" width="2rem" />} />
       </Link>
@@ -42,7 +42,7 @@ export const Navbar = () => {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </button>
-      <div className={`${isMenuOpen ? "" : "hidden"}   w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
+      <div className={`${isMenuOpen ? "" : "hidden"} w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
         <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
           <div
             className="text-sm lg:inline-flex lg:w-auto w-full lg:px-6 py-2 rounded text-gray-50 font-light items-center justify-center hover:text-white"
