8be88c69135395c5f9ac3d7b1f905e2cffe8413c
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-01-12 16:10:35 +0100

    feat: shows domain presentation in player page

--- client/src/pages/PlayerPage.tsx
+++ client/src/pages/PlayerPage.tsx
@@ -3,12 +3,23 @@ import { Game } from "@big5ish/types";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useParams } from "react-router-dom";
+import { DomainPresentation } from "../components/DomainPresentation";
 import { PlayerList } from "../components/PlayerList";
+import { useEmojis } from "../hooks/useEmojis";
 
 const gameQuery = gql`
   query game($gameId: ID!) {
     game(gameId: $gameId) {
       status
+      currentStep
+      steps {
+        type
+        duration
+        domain {
+          domain
+          title
+        }
+      }
     }
   }
 `;
@@ -18,6 +29,8 @@ export const PlayerPage = () => {
 
   const { data, loading, error } = useQuery<{ game: Game }>(gameQuery, { variables: { gameId }, pollInterval: 500 });
 
+  const emojis = useEmojis(data);
+
   if (loading) return <FontAwesomeIcon className={`animate-spin text-gray-200`} icon={faSpinner} />;
 
   if (error || !gameId || !data?.game) {
@@ -25,14 +38,20 @@ export const PlayerPage = () => {
   }
 
   return (
-    <div className="h-full flex flex-col items-center justify-center">
+    <div className="w-full h-full flex flex-col items-center">
       {data.game.status === "CREATED" && (
         <>
           <p className="text-xl font-bold text-white">Waiting for the game to start...</p>
           <PlayerList gameId={gameId} />
         </>
       )}
-      {data.game.status === "STARTED" && <p className="text-white">Game started!</p>}
+      {data.game.status === "STARTED" && (
+        <DomainPresentation
+          currentStep={data.game.currentStep}
+          stepCount={data.game.steps.length}
+          domain={{ ...data.game.steps[data.game.currentStep].domain, emojis }}
+        />
+      )}
     </div>
   );
 };
