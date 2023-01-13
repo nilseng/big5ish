36295df90dd61f75b07fd0edf53e7869fdc541de
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-01-13 13:22:29 +0100

    refactor: updates startGame to account for new types

--- server/src/use-cases/startGame.ts
+++ server/src/use-cases/startGame.ts
@@ -1,20 +1,23 @@
 import { Game } from "@big5ish/types";
 import { GameGateway } from "../gateways/game.gateway";
+import { isTimeBoundStep } from "../utils/typeGuards";
 
 export const startGame = ({ gameId, gameGateway }: { gameId: string; gameGateway: GameGateway }) => {
   const game = gameGateway.getGame(gameId);
   if (!game) throw Error("Game not found - could not be started.");
-  if (shouldScheduleNextStep(game)) scheduleNextStep(game, gameGateway);
+  scheduleNextStep(game, gameGateway);
   return gameGateway.startGame(gameId);
 };
 
 const shouldScheduleNextStep = (game: Game, currentStep = 0): boolean => {
-  return !!game.steps[currentStep].duration && game.currentStep < game.steps.length - 1;
+  return isTimeBoundStep(game.steps[currentStep]) && game.currentStep < game.steps.length - 1;
 };
 
-const scheduleNextStep = (game: Game, gameGateway: GameGateway, currentStep = 0) => {
+const scheduleNextStep = (game: Game, gameGateway: GameGateway, currentStepIndex = 0) => {
+  const currentStep = game.steps[currentStepIndex];
+  if (!isTimeBoundStep(currentStep)) return;
   setTimeout(() => {
-    gameGateway.setStep(game.id, currentStep + 1);
-    if (shouldScheduleNextStep(game, currentStep + 1)) scheduleNextStep(game, gameGateway, currentStep + 1);
-  }, game.steps[currentStep].duration);
+    gameGateway.setStep(game.id, currentStepIndex + 1);
+    if (shouldScheduleNextStep(game, currentStepIndex + 1)) scheduleNextStep(game, gameGateway, currentStepIndex + 1);
+  }, currentStep.duration);
 };
