b5fb5344f24d0599b1833f6e02919581ab17e9e1
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-01-16 22:57:03 +0100

    fix: makes player ratings optional

--- common/types/src/index.d.ts
+++ common/types/src/index.d.ts
@@ -12,7 +12,7 @@ export interface GameBase {
 export interface MultiplayerGame extends GameBase {
   currentStep: number;
   steps: Step[];
-  playerRatings: {
+  playerRatings?: {
     [domainId in keyof DomainId]: {
       [ratedBy: string]: {
         [playerId: string]: {
