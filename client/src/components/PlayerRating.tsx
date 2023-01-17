2a0812b8bf4ec108e8b24a1e8c6c80eb27fb1d77
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-01-17 13:38:42 +0100

    feat: domain score guesses in frontend

--- client/src/components/PlayerRating.tsx
+++ client/src/components/PlayerRating.tsx
@@ -1,16 +1,51 @@
-import { Game } from "@big5ish/types";
+import { gql, useMutation } from "@apollo/client";
+import { Game, Player } from "@big5ish/types";
 import { faSpinner, faUserNinja } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useCurrentStep } from "../hooks/useCurrentStep";
 import { useOtherPLayers } from "../hooks/useOtherPlayers";
 import { ErrorMsg } from "./ErrorMsg";
 
+const defaultScore = 3;
+
+const guessDomainScoresMutation = gql`
+  mutation guessDomainScores($input: DomainScoreGuessInput!) {
+    guessDomainScores(input: $input) {
+      id
+    }
+  }
+`;
+
+const createDomainScoreGuessMap = (players?: Player[]) => {
+  const guesses: { [playerId: string]: number } = {};
+  players?.forEach((p) => {
+    guesses[p.id] = defaultScore;
+  });
+  return guesses;
+};
+
 export const PlayerRating = ({ view, game }: { view: "single" | "common"; game: Game }) => {
   const otherPlayers = useOtherPLayers(game);
   const currentStep = useCurrentStep({ game: game });
+  const [guesses, setGuesses] = useState<{ [playerId: string]: number } | undefined>();
+  useEffect(() => setGuesses(createDomainScoreGuessMap(otherPlayers)), [otherPlayers]);
 
-  if (currentStep?.type !== "playerRating") return <ErrorMsg msg={"Troubles 😥⚙️"} />;
+  const [guessDomainScores, { error }] = useMutation(guessDomainScoresMutation);
+
+  if (error || currentStep?.type !== "playerRating") return <ErrorMsg msg={"Troubles 😥⚙️"} />;
+
+  const guessScores = async () => {
+    await guessDomainScores({
+      variables: {
+        input: {
+          domainId: currentStep.domainId,
+          guessedBy: localStorage.getItem("playerId"),
+          guesses: Object.keys(guesses!).map((playerId) => ({ playerId, score: guesses![playerId] })),
+        },
+      },
+    });
+  };
 
   return (
     <div className="w-full max-w-md pb-6">
@@ -31,7 +66,7 @@ export const PlayerRating = ({ view, game }: { view: "single" | "common"; game: 
       {view === "single" && otherPlayers && (
         <>
           <div className="grid grid-cols-3 gap-6 p-6">
-            {otherPlayers.map((player) => (
+            {otherPlayers?.map((player) => (
               <Fragment key={player.id}>
                 <div className="flex flex-col items-center">
                   <FontAwesomeIcon icon={faUserNinja} size={"2x"} />
@@ -44,13 +79,18 @@ export const PlayerRating = ({ view, game }: { view: "single" | "common"; game: 
                   min={1}
                   max={5}
                   step={1}
-                  defaultValue={3}
+                  value={guesses?.[player.id] ?? defaultScore}
+                  onChange={(e) => {
+                    setGuesses((g) => ({ ...g, [player.id]: +e.target.value }));
+                  }}
                 />
               </Fragment>
             ))}
           </div>
           <div className="w-full p-6">
-            <button className="bg-success-400 float-right rounded-lg px-4 py-2">Ready</button>
+            <button className="bg-success-400 float-right rounded-lg px-4 py-2" onClick={() => guessScores()}>
+              Ready
+            </button>
           </div>
         </>
       )}
