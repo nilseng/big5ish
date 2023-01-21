27c92422ebb9fb8deaef732d6ba5e2f553ea0fa6
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-01-21 14:37:30 +0100

    nit: summary step styling

--- client/src/components/SummaryStep.tsx
+++ client/src/components/SummaryStep.tsx
@@ -6,9 +6,10 @@ export const SummaryStep = ({ game }: { game: Game }) => {
   const gameResults = useMemo(() => calculateResults(game), [game]);
   return (
     <>
+      <h2 className="text-3xl text-center p-6">Result Summary</h2>
       {game.players.map((player) => (
         <Fragment key={player.id}>
-          <h2>{player.nickname}</h2>
+          <h3 className="text-2xl pt-6">{player.nickname}</h3>
           {Object.values(domains).map((domainId) => (
             <p key={domainId}>
               Domain {domainId}: {gameResults[player.id][domainId]?.avgScore?.toFixed(2)}
