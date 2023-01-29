6666d036220329ffdf5e4ba0bcec00a00090d8c0
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-01-29 11:14:14 +0100

    feat: next step button on domain summary game page

--- client/src/components/DomainSummaryStep.tsx
+++ client/src/components/DomainSummaryStep.tsx
@@ -1,8 +1,17 @@
 import { getFacet } from "@alheimsins/b5-result-text";
+import { gql, useMutation } from "@apollo/client";
 import { DomainSummaryStep as Step, Game } from "@big5ish/types";
 import { useMemo } from "react";
 import { language } from "../config";
 import { calculateDomainResults } from "../utils/gameUtils";
+
+const setNextStepMutation = gql`
+  mutation setNextStep($gameId: ID!) {
+    setNextStep(gameId: $gameId) {
+      id
+    }
+  }
+`;
 
 export const DomainSummaryStep = ({ game, currentStep }: { game: Game; currentStep: Step }) => {
   const playerResults = useMemo(
@@ -16,8 +25,10 @@ export const DomainSummaryStep = ({ game, currentStep }: { game: Game; currentSt
 
   const groupResults = useMemo(() => calculateDomainResults({ game, domain: currentStep.domain }), [game, currentStep]);
 
+  const [setNextStep] = useMutation(setNextStepMutation);
+
   return (
-    <>
+    <div className="w-full max-w-md pb-6">
       <h2 className="text-3xl text-center p-6">{currentStep.domain.title} Results</h2>
       <p className="text-xl font-bold">Overall: {groupResults.avgScore?.toFixed(2)}</p>
       {Object.keys(groupResults.facets ?? {}).map((f) => (
@@ -26,6 +37,12 @@ export const DomainSummaryStep = ({ game, currentStep }: { game: Game; currentSt
           {groupResults?.facets?.[+f]?.toFixed(2)}
         </p>
       ))}
-    </>
+      <button
+        className="bg-success-400 float-right rounded-lg font-bold px-4 py-2"
+        onClick={() => setNextStep({ variables: { gameId: game.id } })}
+      >
+        Next
+      </button>
+    </div>
   );
 };
