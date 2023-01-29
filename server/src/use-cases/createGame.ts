3362bb77513757243c30ee784375c6d510d1ce02
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-01-29 10:31:26 +0100

    feat: adds domains summary step when creating game and refactors create game function

--- server/src/use-cases/createGame.ts
+++ server/src/use-cases/createGame.ts
@@ -1,6 +1,14 @@
 import { getItems } from "@alheimsins/b5-johnson-120-ipip-neo-pi-r";
 import { getTemplate as getDomains } from "@alheimsins/b5-result-text";
-import { DomainPresentationStep, QuestionStep, Step, StepType } from "@big5ish/types";
+import {
+  Domain,
+  DomainPresentationStep,
+  DomainScoreGuessStep,
+  DomainSummaryStep,
+  QuestionStep,
+  Step,
+  StepType,
+} from "@big5ish/types";
 import { GameGateway } from "../gateways/game.gateway";
 
 export const createGame = ({
@@ -18,31 +26,50 @@ export const createGame = ({
 
 const getSteps = ({ language = "en" }: { language?: string }): Step[] => {
   const [A, C, E, N, O] = getDomains(language).sort((a, b) => (a.domain < b.domain ? -1 : 1));
-  const questions = getItems();
   const sortedDomains = [N, E, O, A, C];
-  const domainSteps: DomainPresentationStep[] = sortedDomains.map((domain) => ({
+  const domainSteps: DomainPresentationStep[] = createDomainSteps(sortedDomains);
+  const steps: Step[] = [];
+  domainSteps.forEach((domainStep) => {
+    const domain = domainStep.domain;
+    steps.push(domainStep);
+    steps.push(createDomainScoreGuessStep(domain));
+    steps.push(...createQuestionSteps(domain));
+    steps.push(createDomainSummaryStep(domain));
+  });
+  steps.push({ type: StepType.SummaryStep });
+  return steps;
+};
+
+const createDomainSteps = (domains: Domain[]): DomainPresentationStep[] => {
+  return domains.map((domain) => ({
     type: StepType.DomainPresentation,
     domain,
     duration: 5000,
   }));
-  const steps: Step[] = [];
-  domainSteps.forEach((step) => {
-    steps.push(step, {
-      type: StepType.DomainScoreGuess,
-      statement: `How do you think the other players score on ${step.domain.title}?`,
-      domainId: step.domain.domain,
-    });
-    steps.push(
-      ...questions
-        .filter((q) => q.domain === step.domain.domain)
-        .map(
-          (q): QuestionStep => ({
-            type: StepType.QuestionStep,
-            question: q,
-          })
-        )
+};
+
+const createDomainScoreGuessStep = (domain: Domain): DomainScoreGuessStep => {
+  return {
+    type: StepType.DomainScoreGuess,
+    statement: `How do you think the other players score on ${domain.title}?`,
+    domainId: domain.domain,
+  };
+};
+
+const createQuestionSteps = (domain: Domain): QuestionStep[] => {
+  return getItems()
+    .filter((q) => q.domain === domain.domain)
+    .map(
+      (q): QuestionStep => ({
+        type: StepType.QuestionStep,
+        question: q,
+      })
     );
-  });
-  steps.push({ type: StepType.SummaryStep });
-  return steps;
+};
+
+const createDomainSummaryStep = (domain: Domain): DomainSummaryStep => {
+  return {
+    type: StepType.DomainSummaryStep,
+    domain,
+  };
 };
