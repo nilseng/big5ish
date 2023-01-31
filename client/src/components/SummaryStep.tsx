14cfc769fec8bf03b0c29ef099768c4d4ffc51e3
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-01-31 17:06:33 +0100

    feat: adds result progress bars to summary step

--- client/src/components/SummaryStep.tsx
+++ client/src/components/SummaryStep.tsx
@@ -1,8 +1,11 @@
 import { getDomain, getFacet } from "@alheimsins/b5-result-text";
 import { Game } from "@big5ish/types";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useMemo } from "react";
 import { language } from "../config";
 import { calculateResults, domains } from "../utils/gameUtils";
+import { ProgressBar } from "./ProgressBar";
 
 export const SummaryStep = ({ game }: { game: Game }) => {
   const gameResults = useMemo(() => calculateResults(game), [game]);
@@ -10,17 +13,24 @@ export const SummaryStep = ({ game }: { game: Game }) => {
     <>
       <h2 className="text-3xl text-center p-6">Result Summary</h2>
       <div key={"group"}>
-        <h3 className="text-2xl pt-6">Group</h3>
+        <h3 className="text-2xl pt-6">
+          Group
+          <FontAwesomeIcon icon={faUsers} className="ml-2" />
+        </h3>
         {Object.values(domains).map((domainId) => (
           <div key={domainId} className="py-2">
             <p className="text-xl font-bold">
-              {getDomain({ domain: domainId, language }).title}: {gameResults.group[domainId]?.avgScore?.toFixed(2)}
+              {getDomain({ domain: domainId, language }).title} {gameResults.group[domainId]?.avgScore?.toFixed(1)}
             </p>
+            <ProgressBar value={gameResults.group[domainId]?.avgScore} max={5} className="h-4 mb-2" />
             {Object.keys(gameResults.group[domainId]?.facets ?? {}).map((f) => (
-              <p key={f}>
-                {getFacet({ domain: domainId, language, facet: +f }).title}:{" "}
-                {gameResults.group[domainId]?.facets?.[+f]?.toFixed(2)}
-              </p>
+              <div key={f} className="pb-1">
+                <p>
+                  {getFacet({ domain: domainId, language, facet: +f }).title}{" "}
+                  {gameResults.group[domainId]?.facets?.[+f]?.toFixed(1)}
+                </p>
+                <ProgressBar value={gameResults.group[domainId]?.facets?.[+f]} max={5} />
+              </div>
             ))}
           </div>
         ))}
@@ -31,14 +41,18 @@ export const SummaryStep = ({ game }: { game: Game }) => {
           {Object.values(domains).map((domainId) => (
             <div key={domainId} className="py-2">
               <p className="text-xl font-bold">
-                {getDomain({ domain: domainId, language }).title}:{" "}
-                {gameResults[player.id][domainId]?.avgScore?.toFixed(2)}
+                {getDomain({ domain: domainId, language }).title}{" "}
+                {gameResults[player.id][domainId]?.avgScore?.toFixed(1)}
               </p>
+              <ProgressBar value={gameResults[player.id][domainId]?.avgScore} max={5} className="h-4 mb-2" />
               {Object.keys(gameResults[player.id][domainId]?.facets ?? {}).map((f) => (
-                <p key={f}>
-                  {getFacet({ domain: domainId, language, facet: +f }).title}:{" "}
-                  {gameResults[player.id][domainId]?.facets?.[+f]?.toFixed(2)}
-                </p>
+                <div key={f} className="pb-1">
+                  <p>
+                    {getFacet({ domain: domainId, language, facet: +f }).title}{" "}
+                    {gameResults[player.id][domainId]?.facets?.[+f]?.toFixed(1)}
+                  </p>
+                  <ProgressBar value={gameResults[player.id][domainId]?.facets?.[+f]} max={5} />
+                </div>
               ))}
             </div>
           ))}
