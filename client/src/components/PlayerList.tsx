3eab0517e4415e08c1f43de72d5552399f4a2446
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-07 14:06:38 +0100

    feat: translates player list

--- client/src/components/PlayerList.tsx
+++ client/src/components/PlayerList.tsx
@@ -1,5 +1,8 @@
 import { gql, useQuery } from "@apollo/client";
 import { Player } from "@big5ish/types";
+import { useContext } from "react";
+import { LocaleContext } from "../App";
+import translations from "./PlayerList.translations.json";
 
 const playersQuery = gql`
   query players($gameId: ID!) {
@@ -11,6 +14,8 @@ const playersQuery = gql`
 `;
 
 export const PlayerList = ({ gameId }: { gameId: string }) => {
+  const { locale } = useContext(LocaleContext);
+
   const { data } = useQuery<{ players: Player[] }, { gameId: string }>(playersQuery, {
     variables: { gameId },
     pollInterval: 500,
@@ -18,9 +23,11 @@ export const PlayerList = ({ gameId }: { gameId: string }) => {
 
   return (
     <div className="flex flex-col items-center w-full max-w-xs pt-10 pb-4">
-      <h2 className="text-gray-50 text-xl pb-4">Players</h2>
+      <h2 className="text-gray-50 text-xl pb-4">{translations[locale].Players}</h2>
       <div className="flex flex-col items-center w-full h-60 max-h-60 overflow-scroll bg-gray-50 bg-opacity-10 rounded-xl p-4">
-        {(!data?.players || data?.players.length === 0) && <p className="text-gray-200">No one has joined yet...</p>}
+        {(!data?.players || data?.players.length === 0) && (
+          <p className="text-gray-200">{translations[locale].NoOneHasJoinedYet}</p>
+        )}
         {data?.players?.map((player) => (
           <div key={player.id} className="text-gray-50 text-sm font-bold py-2">
             {player.nickname}
