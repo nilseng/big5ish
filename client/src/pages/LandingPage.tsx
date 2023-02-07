94e342a5537e763c87115ee652c99fa3bd0383da
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-07 13:41:02 +0100

    feat: adds buttons for setting locale

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -8,6 +8,7 @@ import { paths } from "../config";
 import translations from "./LandingPage.translations.json";
 
 export const roomIDLength = 6;
+const boldGradientTextClass = "font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-blue-500";
 
 const createRoomMutation = gql`
   mutation createGame($id: ID!) {
@@ -19,7 +20,7 @@ export const LandingPage = () => {
   const [gameId] = useState<string>(nanoid(roomIDLength));
   const navigate = useNavigate();
 
-  const { locale } = useContext(LocaleContext);
+  const { locale, setLocale } = useContext(LocaleContext);
 
   const [createRoom, { error }] = useMutation(createRoomMutation);
 
@@ -31,15 +32,27 @@ export const LandingPage = () => {
 
   return (
     <div className="h-full w-full flex justify-center items-center p-4">
-      <div className="h-96 w-96 flex justify-center items-center bg-light rounded-3xl">
+      <div className="h-96 w-96 flex flex-col justify-center items-center bg-light rounded-3xl">
         <NeuButton
           type="light"
-          textClassName="text-gray-700 font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-blue-500"
+          textClassName={`text-gray-700 text-xl ${boldGradientTextClass}`}
           text={translations[locale].start}
           style={{ height: "10rem", width: "10rem", padding: "2.5rem" }}
           asyncAction={initiateRoom}
           action={() => navigate(`${paths.waitingRoom}/${gameId}`)}
         />
+        <div className="grid grid-cols-2 gap-4 place-items-center pt-4">
+          {locale === "no" ? (
+            <p className={`${boldGradientTextClass}`}>Nor</p>
+          ) : (
+            <NeuButton type="light" text="Nor" className="h-16 w-16" action={() => setLocale("no")} />
+          )}
+          {locale === "en" ? (
+            <p className={`${boldGradientTextClass}`}>Eng</p>
+          ) : (
+            <NeuButton type="light" text="Eng" className="h-16 w-16" action={() => setLocale("en")} />
+          )}
+        </div>
       </div>
     </div>
   );
