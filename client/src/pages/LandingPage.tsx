5435b688377038db6703f4c3fd443656c6c857d7
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-07 12:41:28 +0100

    feat: translates landing

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import { gql, useMutation } from "@apollo/client";
 import { nanoid } from "nanoid";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { LocaleContext } from "../App";
 import { NeuButton } from "../components/NeuButton";
 import { paths } from "../config";
+import translations from "./LandingPage.translations.json";
 
 export const roomIDLength = 6;
 
@@ -17,13 +19,15 @@ export const LandingPage = () => {
   const [gameId] = useState<string>(nanoid(roomIDLength));
   const navigate = useNavigate();
 
+  const { locale } = useContext(LocaleContext);
+
   const [createRoom, { error }] = useMutation(createRoomMutation);
 
   const initiateRoom = async () => {
     await createRoom({ variables: { id: gameId } });
   };
 
-  if (error) return <p className="text-xl font-bold text-gray-50">Oh, no! Something went wrong! 😭</p>;
+  if (error) return <p className="text-xl font-bold text-gray-50">{translations[locale].errorMsg}</p>;
 
   return (
     <div className="h-full w-full flex justify-center items-center p-4">
@@ -31,7 +35,7 @@ export const LandingPage = () => {
         <NeuButton
           type="light"
           textClassName="text-gray-700 font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-blue-500"
-          text="START"
+          text={translations[locale].start}
           style={{ height: "10rem", width: "10rem", padding: "2.5rem" }}
           asyncAction={initiateRoom}
           action={() => navigate(`${paths.waitingRoom}/${gameId}`)}
