7a5c51be04e5a030be31eb999752b9eb684a3f8b
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-07 11:22:04 +0100

    feat: adds locale context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { createContext, Dispatch, SetStateAction, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { paths } from "./config";
@@ -8,21 +9,33 @@ import { NicknamePage } from "./pages/NicknamePage";
 import { PlayerPage } from "./pages/PlayerPage";
 import { WaitingRoomPage } from "./pages/WaitingRoomPage";
 
+const defaultLocale = "no";
+
+export const LocaleContext = createContext<{ locale: string; setLocale: Dispatch<SetStateAction<string>> }>({
+  locale: defaultLocale,
+  setLocale: () => {},
+});
+
 const App = () => {
+  const [locale, setLocale] = useState(defaultLocale);
+  const [localeContext] = useState({ locale, setLocale });
+
   return (
-    <div className="h-full max-h-full w-screen overflow-hidden">
-      <Navbar />
-      <div className="h-[calc(100%-6rem)] w-full flex flex-col items-center overflow-auto text-white">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path={`${paths.waitingRoom}/:gameId`} element={<WaitingRoomPage />} />
-          <Route path={`${paths.gameIdForm}`} element={<GameIdPage />} />
-          <Route path={`${paths.nicknameForm}/:gameId`} element={<NicknamePage />} />
-          <Route path={`${paths.playerPage}/:gameId`} element={<PlayerPage />} />
-          <Route path={`${paths.gamePage}/:gameId`} element={<GamePage />} />
-        </Routes>
+    <LocaleContext.Provider value={localeContext}>
+      <div className="h-full max-h-full w-screen overflow-hidden">
+        <Navbar />
+        <div className="h-[calc(100%-6rem)] w-full flex flex-col items-center overflow-auto text-white">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path={`${paths.waitingRoom}/:gameId`} element={<WaitingRoomPage />} />
+            <Route path={`${paths.gameIdForm}`} element={<GameIdPage />} />
+            <Route path={`${paths.nicknameForm}/:gameId`} element={<NicknamePage />} />
+            <Route path={`${paths.playerPage}/:gameId`} element={<PlayerPage />} />
+            <Route path={`${paths.gamePage}/:gameId`} element={<GamePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </LocaleContext.Provider>
   );
 };
 
