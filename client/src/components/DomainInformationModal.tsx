e28e607c816c695c78099284df105b838e52fa5d
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-15 14:52:58 +0100

    feat: adds close icon to domain info modal

--- client/src/components/DomainInformationModal.tsx
+++ client/src/components/DomainInformationModal.tsx
@@ -1,4 +1,6 @@
 import { Domain } from "@big5ish/types";
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Dispatch, SetStateAction } from "react";
 
 export const DomainInformationModal = ({
@@ -17,6 +19,7 @@ export const DomainInformationModal = ({
       onClick={() => setIsOpen(false)}
     >
       <div className="min-h-[50%] max-h-full w-full max-w-md overflow-y-auto bg-white rounded-2xl text-gray-700 p-6">
+        <FontAwesomeIcon className="float-right cursor-pointer" icon={faWindowClose} />
         <h1 className="text-2xl pb-4">{domain.title}</h1>
         {domain.description.split(/<br\s?\/>/).map((text, i) => (
           <p key={i} className="pb-2">
