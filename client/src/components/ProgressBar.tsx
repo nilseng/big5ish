0e9990caf828a2d018f12fa0cd7ff246cbb06bac
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-15 14:44:46 +0100

    feat: zero indexes progress bar visual

--- client/src/components/ProgressBar.tsx
+++ client/src/components/ProgressBar.tsx
@@ -1,9 +1,17 @@
+import { useMemo } from "react";
+
 export const ProgressBar = ({ value, max, className }: { value?: number; max: number; className?: string }) => {
-  if (!(value && value !== 0) || !max) return null;
+  const { normalizedValue, normalizedMax } = useMemo(
+    () => ({ normalizedValue: value ? value - 1 : NaN, normalizedMax: max - 1 }),
+    [value, max]
+  );
   return (
     <div className={`relative w-full rounded-full ${className ?? "h-2.5"}`}>
       <div className="absolute top-0 bg-gray-200 opacity-10 w-full h-full rounded-full"></div>
-      <div className="absolute top-0 bg-white h-full rounded-full" style={{ width: `${(100 * value) / max}%` }}></div>
+      <div
+        className="absolute top-0 bg-white h-full rounded-full"
+        style={{ width: `${(100 * normalizedValue) / normalizedMax}%` }}
+      ></div>
     </div>
   );
 };
